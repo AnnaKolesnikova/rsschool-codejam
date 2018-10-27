6a149b291737e853cd0a242f9f4a93f222157c7f
Anna <ani.kolesnikova@gmail.com>
2018-10-28 00:55:36 +0300

    feat: add solution sumOfOthers

---
+++ sumOfOther.js
@@ -0,0 +1,15 @@
+function sumOfOthers(array) {
+  var result = [];
+  for(var i = 0; i < array.length; i++) {
+    var sum = 0;
+    for(var j = 0; j < array.length; j++) {
+       if(j != i) {
+         sum += array[j];
+       }
+    }
+    result.push(sum);
+  }
+  return result;
+ }
+
+ //sumOfOthers([2, 3, 4, 1]);
